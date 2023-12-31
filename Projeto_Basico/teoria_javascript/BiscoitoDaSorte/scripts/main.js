77ea499375c81c797cb1ee2b277d5c724d4d382f
DouglasSoares1203 <douglashardware13@gmail.com>
2023-12-30 23:03:07 -0300

    Biscoito da sorte

---
+++ Projeto_Basico/teoria_javascript/BiscoitoDaSorte/scripts/main.js
@@ -0,0 +1,36 @@
+import { Messages } from "./messages.js";
+import { OpenCookie } from "./controllers.js";
+
+const cookie = document.querySelector("#cookie");
+const openedCookie = document.querySelector("#opened-cookie");
+const buttonResetCookie = document.querySelector(".reset-cookie");
+
+function clickAwait() {
+  setTimeout(function () {
+    OpenCookie.getLuckyMessage();
+    OpenCookie.open();
+  }, 3000);
+}
+
+window.addEventListener("keypress", (e) => {
+  if (openedCookie.classList.contains("hide") && e.key === "Enter") {
+    cookie.click();
+  }
+
+  if (cookie.classList.contains("hide") && e.key === "Enter") {
+    buttonResetCookie.click();
+  }
+});
+
+cookie.addEventListener("click", () => {
+  cookie.classList.add("shake");
+  clickAwait();
+});
+
+buttonResetCookie.addEventListener("click", () => {
+  OpenCookie.reset();
+  OpenCookie.titleBox.innerText = Messages.title;
+  OpenCookie.subtitleBox.innerText = Messages.subtitle;
+});
+
+export { cookie, openedCookie, buttonResetCookie };
