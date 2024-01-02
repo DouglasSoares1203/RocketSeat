c7e67cd22fb58988a7f4213486913a87dc7b86f1
DouglasSoares1203 <douglashardware13@gmail.com>
2024-01-02 11:14:09 -0300

    CALCULO IMC

---
+++ Projeto_Basico/teoria_javascript/CalcularIMC/js/alert-error.js
@@ -0,0 +1,11 @@
+export const AlertError = {
+  message: document.querySelector(".alert-error"),
+
+  open() {
+    AlertError.message.classList.add("open");
+  },
+
+  close() {
+    AlertError.message.classList.remove("open");
+  },
+};
